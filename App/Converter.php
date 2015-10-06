073c24c6b9cdf4c1e9c5eb5ce085d42ac7ee6732
ganeshjaiwal93 <ganesh.jaiwal@vshsolutions.com>
2015-10-07 02:43:52 +0530

    Class Structure is changed and main core ogic for convertion is written

--- App/Converter.php
+++ App/Converter.php
@@ -2,17 +2,10 @@
 namespace ConverterApp;
 class Converter
 {
-	
-
-
-
-
-
-
-
-
-
-
-
-	
+   public function converter($object,$value)
+    {
+        $power = $object->$fromUnit - $object->$toUnit;
+        $convertedValue = pow($value,$power);
+        return $convertedValue;
+    }
 }
--- App/Temperature.php
+++ App/Temperature.php
@@ -2,12 +2,31 @@
 namespace ConverterApp;
 class Temperature extends Imperial
 {
-  
+  public function convertFahrenheitToCelsius($unit)
+  {
+    $this->argumentValidator($unit);
+    $Celsius = (5/9)*($unit - 32);
+    return $Celsius;
+  }
 
+  public function convertCelsiusToFahrenheit($unit)
+  {
+    $this->argumentValidator($unit);
+    $Fahrenhit = ($unit*9/5) + 32;
+    return $Fahrenhit;
+  }
 
+  public function convertKelvinToCelsius($unit)
+  {
+    $this->argumentValidator($unit);
+    $Celsius = $unit - 273.15;
+    return $Celsius;
+  }
 
-
-
-
-
+  public function convertCelsiusToKelvin($unit)
+  {
+    $this->argumentValidator($unit);
+    $Kelvin = $unit + 273.15;
+    return $Kelvin;
+  }
 }
