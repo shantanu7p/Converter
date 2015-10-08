84856b63c6e594e73a02862621b3e28084dad97c
ganeshjaiwal93 <ganesh.jaiwal@vshsolutions.com>
2015-10-08 14:19:20 +0530

    Validation Functions are writen in Other classes

--- App/Metric.php
+++ App/Metric.php
@@ -2,10 +2,14 @@
 namespace ConverterApp;
 class Metric extends Unit
 {
-
-
-
-
-
-
+  public function argumentValidator($argument)
+  {
+    if (is_string($argument))
+    {
+      throw new \InvalidArgumentException("Please Enter valid Argument");
+    } elseif (is_null($argument))
+    {
+      throw new \InvalidArgumentException("Please Enter valid Argument");
+    }
+  }
 }
